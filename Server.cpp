072fc41a0cdbdee9a07e5a62d2ad5ae0c27c290d
esther <esthers@live.dk>
2024-04-04 15:08:56 +0200

    -

--- Server.cpp
+++ Server.cpp
@@ -30,15 +30,16 @@ void  Server::initSocket( void )
     fcntl(serversock.fd, F_SETFL, O_NONBLOCK); // Set server socket to non-blocking mode
 }
 
-void  Server::checkConnections()
+int  Server::checkConnections()
 {
-  //poll allows to monitor multiple fd's and prevent blocking
-  if (poll(&_sockets[0], _sockets.size(), -1) < 0) {
+  //poll allows to monitor multiple fd's for events
+  int newEvents = poll(&_sockets[0], _sockets.size(), -1);
+  if (newEvents  < 0) {
       std::perror("poll");
       exit(EXIT_FAILURE);
   }
-
-  if (_sockets[0].revents & POLLIN) { // Check if there's a new connection on the server socket
+  // Check if there's a new connection on the server socket
+  if (_sockets[0].revents & POLLIN) { 
   	unsigned int addrlen = sizeof(sockaddr);
     int new_socket = accept(_sockets[0].fd, (struct sockaddr *)&_sockaddr, (socklen_t*)&addrlen);
     if (new_socket < 0) {
@@ -54,6 +55,7 @@ void  Server::checkConnections()
     newfd.events = POLLIN;
     _sockets.push_back(newfd);
   }
+  return newEvents;
 }
 
 void  Server::disconnectClient(int i, ssize_t bytesRead) {
@@ -64,13 +66,44 @@ void  Server::disconnectClient(int i, ssize_t bytesRead) {
         close(_sockets[i].fd);
         _sockets.erase(_sockets.begin() + i);
     }
+    else {
+       perror("read");
+       exit(EXIT_FAILURE);
+    }
 }
 
 
-void  Server::sendToClient(int i, char buffer[]) {
+int  Server::sendToClient(int i, char buffer[]) {
   std::cout << "Received: " << buffer << "\n" << std::endl;
-  std::string response = "Good talking to you\n";
-  send(_sockets[i].fd, response.c_str(), response.size(), 0);
+
+    // Read HTML file
+    std::ifstream file(HTML_FILE, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << HTML_FILE << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    char* buffer = new char[size];
+    file.read(buffer, size);
+    file.close();
+
+    // Send HTTP headers
+    std::string headers = "HTTP/1.1 200 OK\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: ";
+    headers += std::to_string(size) + "\r\n\r\n";
+
+    std::ostringstream oss;
+    oss << size + "\r\n\r\n";
+    send(_sockets[i].fd, headers.c_str(), headers.size(), 0);
+
+    // Send HTML content
+    send(_sockets[i].fd, buffer, size, 0);
+
+    // Clean up
+    delete[] buffer;
+
 }
 
 Server::Server()
@@ -78,17 +111,18 @@ Server::Server()
   initSocket();
 
   while (true) {
-    checkConnections();
-
+    if (checkConnections() < 1) //look for new connections to the server
+      continue;
     for (int i = _sockets.size() - 1; i >= 1; --i) {
       if (_sockets[i].revents & POLLIN) { // Check if there's data to read on client socket
         char buffer[100];
         ssize_t bytesRead = read(_sockets[i].fd, buffer, sizeof(buffer));
         if (bytesRead <= 0) {
-            disconnectClient(i, bytesRead);
+          disconnectClient(i, bytesRead);
         } else {
           sendToClient(i, buffer);
         }
+
       }
     }
   }
@@ -109,7 +143,9 @@ Server::Server( const Server & src )
 
 Server::~Server()
 {
-    close(_sockets[0].fd);
+     for (size_t i = _sockets.size(); i >= 0; --i) {
+        close(_sockets[i].fd);
+    }
 }
 
 
