ad8b20819c8e51ae3a481d360af71495b72234d2
Jakob Schott <jschott@student.42berlin.de>
2024-04-05 15:49:55 +0200

    config gets separated in server blocks

--- srcs/config_parser/read_config.cpp
+++ srcs/config_parser/read_config.cpp
@@ -6,7 +6,7 @@
 /*   By: jschott <jschott@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:20:40 by jschott           #+#    #+#             */
-/*   Updated: 2024/04/05 14:08:35 by jschott          ###   ########.fr       */
+/*   Updated: 2024/04/05 15:49:25 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <algorithm>
 #include <cctype>
 #include <stack>
+#include <vector>
 
 bool isBalanced(std::stringstream& ss) {
     std::stack<char> stack;
@@ -39,7 +40,6 @@ bool isBalanced(std::stringstream& ss) {
 }
 
 void	readFile2Buffer (std::string filename){
-	std::string 		buffer;
 	std::stringstream	bufferstream;
 	std::ifstream		input(filename);
 	
@@ -53,35 +53,63 @@ void	readFile2Buffer (std::string filename){
 	bufferstream << input.rdbuf();
 	input.close();
 
-	//Delete spaces & tabs -> USE INPUTSTRINGSTREAM LIBRARY
-	while (bufferstream.good()){
-		char c;
-		bufferstream.get(c);
-		if (!std::isspace(c))
-			buffer += c;
-	}
-	bufferstream.str("");
-	bufferstream << buffer;
-
 	//Clean input from spaces and comments
-	while (std::getline(bufferstream, buffer)) {
-		
-		//Delete comments
-		std::size_t pos = buffer.find('#');
-		if (pos != std::string::npos) {
-				buffer.erase(pos);
-		}
+	std::string 		buffer = "";
+	std::string			line;
+	std::stringstream	buffing;
+	while (std::getline(bufferstream, line)) {
 		
 		//delete spaces
-		buffer.erase(std::remove_if(buffer.begin(), buffer.end(), isspace), buffer.end());
+		line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
+
+		//Delete comments
+		std::size_t pos = line.find('#');
+		if (pos != std::string::npos) {
+				line.erase(pos);
+		}
+		
+		buffing << line << std::endl;
+		// buffer += line + "\n";
 	}
-	bufferstream.str("");
-	bufferstream << buffer;
+	bufferstream.clear();
+	bufferstream.str(buffer);
+
+	// std::cout << "POST CLEANING CONTENT:" << std::endl \
+				<< buffing.str() << std::endl << std::endl;
 	
+
+	//make sure that every curly braket will be closed
 	if (!isBalanced(bufferstream)) {
 		std::cerr << "Brakets are not balanced" << std::endl;
 		exit (1);
 	}
 	//check server keyword
+	std::vector<std::string>	servers;
+	std::getline(buffing, buffer);
+		// std::cout << buffer;
+	while (std::getline(buffing, line)){
+		// std::cout << "hello";
+		if (!line.compare(0,7,"server{")){
+			servers.push_back(buffer);
+			std::cout << "SERVER BLOCK:" << std::endl << buffer << std::endl << std::endl;
+			buffer = line + '\n';
+			line.clear();
+		}
+		else
+			buffer += line;
+	}
+	servers.push_back(buffer);
+	// std::cout << buffer;
+
 	//hand over server block to server parser
+}
+
+int main (int argc, char** argv){
+
+	if (argc < 2)
+		return (1);
+
+	readFile2Buffer(argv[1]);
+
+	return (0);
 }
