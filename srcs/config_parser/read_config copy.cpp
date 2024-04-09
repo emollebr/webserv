d2f2e9c8dd3dcdcc794cf2013d9748d6e04037a3
Jakob Schott <jschott@student.42berlin.de>
2024-04-09 16:49:33 +0200

    fixed and rearranged code to proceed with analyzing

--- srcs/config_parser/read_config copy.cpp
+++ srcs/config_parser/read_config copy.cpp
@@ -6,35 +6,63 @@
 /*   By: jschott <jschott@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:20:40 by jschott           #+#    #+#             */
-/*   Updated: 2024/04/09 12:03:03 by jschott          ###   ########.fr       */
+/*   Updated: 2024/04/09 16:48:58 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#define TEXT_NOFORMAT	"\e[0m"
+#define TEXT_BOLD		"\e[1m"
+#define TEXT_UNDERLINE	"\e[4m"
+
+#define COLOR_STANDARD	"\033[0m"
+#define	COLOR_ERROR		"\033[31m"
+#define COLOR_WARNING	"\033[33m"
+#define COLOR_SUCCESS	"\033[92m"
+#define COLOR_FOCUS		"\e[35m"
 
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
 #include <cctype>
 #include <cstring>
 #include <stack>
 #include <vector>
 
+//NEEDS TO GO IN HEADER FILE
+enum class TokenType {
+    Server,
+    Listen,
+    Host,
+    Location,
+    Root,
+	Variable
+};
+
+//Check if all brakets and quotes are closed correctly
 bool isBalanced(std::stringstream& ss) {
     std::stack<char> stack;
     char c;
-
-    while (ss.get(c)) {
-        if (c == '{') {
-            stack.push(c);  // Push open brace onto the stack
-        } else if (c == '}') {
-            if (stack.empty()) {
-                return false;  // Found a closing brace without a corresponding open brace
-            } else {
-                stack.pop();  // Pop corresponding open brace from the stack
-            }
-        }
-    }
+	std::string open = "{\'\"";
+	std::string close = "}\'\"";
+	
+	for (size_t i = 0; i < strlen(open.c_str()); i++) {
+		std::cout << "checking for: " << open[i] << " and " << close[i] << std::endl;
+		while (ss.get(c)) {
+			if (c == open[i]) {
+				stack.push(c);  // Push open brace onto the stack
+			}
+			else if (c == close[i]) {
+				if (stack.empty())
+					return false;  // Found a closing brace without a corresponding open brace
+				else
+					stack.pop();  // Pop corresponding open brace from the stack
+			}
+		}
+		
+	}
 
     // Check if there are any unclosed braces left on the stack
     return stack.empty();
@@ -46,33 +74,16 @@ bool isBalanced(std::stringstream& ss) {
 	
 } */
 
-void	readFile2Buffer (std::string filename){
-	std::stringstream	bufferstream;
-	std::ifstream		input(filename);
-	
-
-	//check that file existst && try to acccess file
-	if (!input.is_open()){
-		std::cerr << "couldn't open file" << std::endl;
-		exit (1);	
-	}
-	//read file to buffer stringstream & close it
-	bufferstream << input.rdbuf();
-	input.close();
-
-	//Clean input from 
-
-	//Clean input from spaces and comments
-	std::string 		buffer = "";
+void	populateTokens(std::stringstream &bufferstream, std::vector<std::string>	&tokens) {
 	std::string			line;
-	std::vector<std::string>	tokens;
 	char				limiter = '\0';
-	
+
 	tokens.push_back("");
 	while (getline(bufferstream, line)) {
 		
 		std::size_t pos;
 	
+		line += '\n';
 		while (!line.empty()) {
 			//if looking for closing limiter copy everything until limiter or whole string
 			if (limiter) {
@@ -87,10 +98,7 @@ void	readFile2Buffer (std::string filename){
 			}
 			if (limiter == '\0') {
 				// copy all tokens separated by space until first special character
-				while (line[0] && !strchr("#;\'\"", line[0])) {
-					// if (!isspace(line[0]))
-						// std::cout << line[0] << " ";
-				
+				while (!strchr("#;\'\"", line[0])) {
 					//create new token if space is found and current token is not empty
 					if (isspace(line[0]) && tokens.back() != "")
 						tokens.push_back("");
@@ -98,7 +106,6 @@ void	readFile2Buffer (std::string filename){
 					else if (line[0] && !isspace(line[0]))
 						tokens.back() += line[0];
 					line = line.erase(0, 1);
-					// std::cout << line << std::endl;
 				}
 
 				// erase comments
@@ -106,27 +113,53 @@ void	readFile2Buffer (std::string filename){
 					line = line.erase();
 
 				//if string is limited within the line, add to line
-				else if (line[0] == '\'') {
+				else {
+					limiter = line[0];
 					//erase limiter
 					line = line.erase(0, 1);
 					//look for closing limiter
-					pos = line.find('\'');
+					pos = line.find(limiter);
 					// save 
-					// std::cout <<  line.substr(0 , pos) << std::endl;
 					tokens.push_back(line.substr(0 , pos));
-					if (pos == std::string::npos) {
-						limiter = '\'';
-					// std::cout << "limiter: " << limiter << std::endl;
+					if (pos == std::string::npos)
 						line = line.erase(0, pos);
-					}
-					else
+					else {
 						line = line.erase();
-					// std::cout << "line: " << line << std::endl;
+						limiter = '\'';
+					}
 				}
 			}
 		}
 	}
+}
 
+void	readFile2Buffer (std::string filename){
+	std::stringstream	bufferstream;
+	std::ifstream		input(filename);
+	
+
+	//check that file existst && try to acccess file
+	if (!input.is_open()){
+		std::cerr << "Error: Couldn't open file" << std::endl;
+		exit (1);	
+	}
+	//read file to buffer stringstream & close it
+	bufferstream << input.rdbuf();
+	std::stringstream braketbuffer;
+	braketbuffer << input.rdbuf();
+	input.close();
+
+	//Check that brakets and quotes are balanced
+	if (!isBalanced(braketbuffer)) {
+		std::cerr << "Error: Brakets or quotes are not balances" << std::endl;
+		exit (1);
+	}
+
+	//Create Tokens from Input
+	std::vector<std::string>	tokens;
+	populateTokens(bufferstream, tokens);
+
+	//Print Tokens [HELPER FUNCTION]
 	for (std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); it++)
 		std::cout << *it << std::endl;
 	
@@ -137,27 +170,18 @@ void	readFile2Buffer (std::string filename){
 	// std::cout << "POST CLEANING CONTENT:" << std::endl << buffing.str() << std::endl << std::endl;
 	
 
-	
-	
-	//check server keyword
-	// std::vector<std::string>	servers;
-	// std::getline(buffing, buffer);
-	// 	// std::cout << buffer;
-	// while (std::getline(buffing, line)){
-	// 	// std::cout << "hello";
-	// 	if (!line.compare(0,7,"server{")){
-	// 		servers.push_back(buffer);
-	// 		std::cout << "SERVER BLOCK:" << std::endl << buffer << std::endl << std::endl;
-	// 		buffer = line + '\n';
-	// 		line.clear();
-	// 	}
-	// 	else
-	// 		buffer += line + "\n";
-	// }
-	// servers.push_back(buffer);
-	// std::cout << buffer;
+	//CHECK FIRST TOKEN
 
-	//hand over server block to server parser
+		//IF SERVER CHECK NEXT TOKEN
+			
+			// IF ISDIRECTIVE CHECK FOR ;
+				//IF FOUND CREATE TUPLE
+			
+			// IF IS BLOCK PARSE BLOCK
+				// FIND OPENING BRAKET
+					// CHECK NEXT TOKEN
+					// PARSE DIRECTIVES base on ;
+			
 }
 
 int main (int argc, char** argv){
