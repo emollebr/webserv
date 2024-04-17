c43f084a3d07946d442058cb8a7d297fe41964be
Jakob Schott <jschott@student.42berlin.de>
2024-04-17 15:40:57 +0200

    configs get saved in objects now! Config is printable to stdout | Validation tbd

--- srcs/config_parser/ParseServer.cpp
+++ srcs/config_parser/ParseServer.cpp
@@ -6,15 +6,15 @@
 /*   By: jschott <jschott@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:33:23 by jschott           #+#    #+#             */
-/*   Updated: 2024/04/16 18:01:20 by jschott          ###   ########.fr       */
+/*   Updated: 2024/04/17 15:36:59 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ParseServer.hpp"
-#include "ParseLocation.hpp"
+// #include "ParseLocation.hpp"
 
 
-ParseServer::ParseServer(){
+void ParseServer::init(){
 	std::string	directives[] = {"listen", "location", "host",
 									"server_name", "error_path"};
 	typedef void (ParseServer::*ParseServerFunction)(std::deque<std::string>::iterator begin, std::deque<std::string>::iterator end);							
@@ -41,6 +41,46 @@ ParseServer::ParseServer(std::vector<std::string> names, std::vector<size_t> lis
 	_ports = listen;
 	_error_path = err;
 	_locations = location;
+}
+
+ParseServer::ParseServer(std::deque<std::string> tokens, std::deque<std::string>::iterator begin, std::deque<std::string>::iterator end){
+	
+	std::cout << "Parsing Server from " << *begin << " to " << *end << std::endl;	
+
+	init();
+	std::deque<std::string>::iterator statementend;
+
+	while (begin < end) {
+		if (*begin == "location"){
+			begin++;
+			while (*begin == "")
+				begin++;
+			std::pair<std::string, std::string> location_block;
+			location_block.first = *begin++;
+			while (*begin == "")
+				begin++;
+			//CHECK FOR OPENING BRAKET AND FIND CLOSING TO PARSE BLOCK
+			if (begin != end && *begin == "{" &&
+					((statementend = getClosingBraket(tokens, begin)) <= tokens.end())){
+				// std::cout << *(begin - 1) << std::endl;
+				ParseLocation *test = new ParseLocation(begin + 1, statementend - 1);
+				std::cout << *test << std::endl;
+				_locations[location_block.first] = new ParseLocation(begin + 1, statementend - 1);
+				// _locations[*(begin - 1)] = new ParseLocation(begin + 1, statementend - 1);
+				begin = statementend + 1;
+			}
+		}
+		// IF IS DIRECTIVE CHECK FOR ';', IF FOUND CREATE TUPLE
+		else if ((statementend = std::find(begin, end, ";")) <= end ) {
+			parseServerDirective(begin, statementend - 1);
+			begin = std::find(begin, end, ";") + 1;
+		}
+
+		else
+			std::cerr << COLOR_ERROR  << "Error: Cannot parse config." << std::endl << COLOR_STANDARD;
+		
+	}
+	std::cout << *this << std::endl;
 }
 
 ParseServer & ParseServer::operator= (ParseServer const & origin) {
@@ -84,7 +124,7 @@ void ParseServer::setServerName(std::vector<std::string> server_names) {
 }
 
 /* GETTER */
-std::vector<size_t> ParseServer::getListenPorts() {
+std::vector<size_t> ParseServer::getListenPorts() const{
 	if (_ports.empty())
 		return std::vector<size_t>();
 	std::vector<std::size_t> ports;
@@ -92,7 +132,7 @@ std::vector<size_t> ParseServer::getListenPorts() {
 	return (ports);
 }
 
-std::map<std::string, ParseLocation*> const ParseServer::getLocations() {
+std::map<std::string, ParseLocation*> const ParseServer::getLocations() const{
 	if (_ports.empty())
 		return std::map<std::string, ParseLocation*>();
 	std::map<std::string, ParseLocation*>	locations;
@@ -115,7 +155,8 @@ std::string	const ParseServer::getErrorPath() const{
 }
 
 void	ParseServer::parseServerDirective(std::deque<std::string>::iterator begin, std::deque<std::string>::iterator end){
-	if (_directives_index.find(*begin) == _directives_index.end())
+	std::cout << "	Parsing Server Directive from " << *begin << " to " << *end << std::endl;
+		if (_directives_index.find(*begin) == _directives_index.end())
 		return ; //THROW EXCEPTION
 	std::map<std::string, void (ParseServer::*)(std::deque<std::string>::iterator, std::deque<std::string>::iterator)>::iterator function = _validation_index.find(*begin);
 	if (function != _validation_index.end()) {
@@ -182,7 +223,8 @@ const ParseServer*	parseServer(std::deque<std::string> tokens, std::deque<std::s
 			//CHECK FOR OPENING BRAKET AND FIND CLOSING TO PARSE BLOCK
 			if (begin != end && *begin == "{" &&
 					((statementend = getClosingBraket(tokens, begin)) <= tokens.end())){
-				ParseLocation(begin + 1, statementend - 1);
+				ParseLocation *test = new ParseLocation(begin + 1, statementend - 1);
+				std::cout << test;
 				begin = statementend + 1;
 			}
 		}
@@ -202,8 +244,14 @@ const ParseServer*	parseServer(std::deque<std::string> tokens, std::deque<std::s
 std::ostream& operator<<(std::ostream& os, const ParseServer& serverconf) {
 	os << "+++START SERVER CONFIGURATION+++" << std::endl << std::endl;
 	// os << "" << _ << std::endl;
-	// os << "" << _ << std::endl;
+	os << "ports:	"  << std::endl;
+	std::vector<size_t> ports = serverconf.getListenPorts();
+	for (std::vector<size_t>::iterator it = ports.begin(); it < ports.end(); it++)
+		os << "			" << *it << std::endl;
 	os << serverconf.getErrorPath() << std::endl;
+	std::map<std::string, ParseLocation*> locations = serverconf.getLocations();
+	for (std::map<std::string, ParseLocation*>::iterator it = locations.begin(); it != locations.end(); it++)
+		os << "	location:	" << (*it).first << std::endl << "			" << *((*it).second) << std::endl;
 	os << "+++ END SERVER CONFIGURATION +++" << std::endl << std::endl;
 	return os;
 	
