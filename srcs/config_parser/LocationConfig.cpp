8be9d71e32b28b90d6d397db2f1992f9e1b02d0d
Jakob Schott <jschott@student.42berlin.de>
2024-04-23 18:09:28 +0200

    about 50% of location validation done

--- srcs/config_parser/LocationConfig.cpp
+++ srcs/config_parser/LocationConfig.cpp
@@ -14,7 +14,7 @@
 
 void LocationConfig::init(){
 	std::string	directives[] = {"root", "index", "methods",
-									"redirect", "CGI", "max_body",
+									"redirect", "CGI", "client_max_body_size",
 									"default_file", "upload_location", 
 									"cgi_extension", "allow_get", "allow_post", 
 									"autoindex"};
@@ -83,7 +83,7 @@ std::vector<std::string> LocationConfig::getMethods() const{
 }
 
 bool LocationConfig::hasMethod(std::string method){
-	if (std::find(_methods_allowed.begin(), _methods_allowed.begin(), method) != _methods_allowed.end())
+	if (std::find(_methods_allowed.begin(), _methods_allowed.end(), method) != _methods_allowed.end())
 		return true;
 	return false;
 }
@@ -169,11 +169,17 @@ void LocationConfig::validateIndeces(tokeniterator begin, tokeniterator end){
 }
 
 void LocationConfig::validateMethods(tokeniterator begin, tokeniterator end){
-	// if (begin == end)
-	// 	return ;
-	while (begin < end)
-		_methods_allowed.push_back(*begin++);
-	
+	std::cout << TEXT_BOLD << "validating methods" << TEXT_NOFORMAT<< std::endl;
+
+	while (begin <= end){
+		std::cout << "checking " << *begin << std::endl;
+		if (!hasMethod(*begin) && 
+			(*begin == "GET" || *begin == "POST" || *begin == "DELETE"))
+			_methods_allowed.push_back(*begin);
+		else if (!hasMethod(*begin))
+			throw InvalidConfigException();
+		begin++;
+	}
 }
 
 void LocationConfig::validateRedirect(tokeniterator begin, tokeniterator end){
@@ -187,8 +193,32 @@ void LocationConfig::validateCGI(tokeniterator begin, tokeniterator end){
 }
 
 void LocationConfig::validateBodySize(tokeniterator begin, tokeniterator end){
-	if (begin == end)
-		_max_body_size = atoi((*begin).c_str());
+	std::cout << TEXT_BOLD << "validating body size" << TEXT_NOFORMAT<< std::endl;
+
+	if (begin == end){
+		// _max_body_size = atoi((*begin).c_str());
+		char* endptrx = NULL;
+
+		unsigned long int body = strtoul((*begin).c_str(), &endptrx, 0);
+		std::string endptr = endptrx;
+
+	// std::cout << "\tbody size:\t" << body << std::endl << "\tfailure:\t" << endptr << std::endl;
+
+	// MISSING INTERNAL LIMIT FOR UPLOAD SIZE THAT MAY NOT BE EXCEEDED TBD
+
+		if (endptr == "" || endptr == "B")
+			_max_body_size = body;
+		else if (endptr == "M" || endptr == "MB")
+			_max_body_size = body * 1024;
+		else if (endptr == "M" || endptr == "MB")
+			_max_body_size = body * 1024 * 1024;
+		else if (endptr == "G" || endptr == "GB")
+			_max_body_size = body * 1024 * 1024 * 1024;
+		else
+			throw InvalidConfigException();
+	}
+	else
+		throw InvalidConfigException();
 }
 
 void LocationConfig::validateDefaultFile(tokeniterator begin, tokeniterator end){
@@ -197,8 +227,18 @@ void LocationConfig::validateDefaultFile(tokeniterator begin, tokeniterator end)
 }
 
 void LocationConfig::validateUploadLocation(tokeniterator begin, tokeniterator end){
-	if (begin == end)
-		_upload_location = *begin;
+	std::cout << TEXT_BOLD << "validating upload" << TEXT_NOFORMAT<< std::endl;
+	if (begin == end){
+		// if (stat((*begin).c_str(), &info) == 0)
+		if (directoryExists(*begin))
+			_root = (*begin);
+		else
+			throw InvalidConfigException();
+			// std::cerr << "Error: root directory " << *begin << " does not exist";
+	}
+	else
+		throw InvalidConfigException();
+		// std::cerr << "Error: Invalid Root Directory" << std::endl;
 }
 
 void LocationConfig::validateCGIExtension(tokeniterator begin, tokeniterator end){
@@ -217,8 +257,19 @@ void LocationConfig::validateAllowPOST(tokeniterator begin, tokeniterator end){
 }
 
 void LocationConfig::validateAutoindex(tokeniterator begin, tokeniterator end){
-	if (begin == end)
-		return ;
+	std::cout << "validation autoindex: " << *begin << std::endl;
+	
+	if (begin == end){
+		if (*begin == "1" || *begin == "on")
+			std::cout << "THIS" << std::endl;
+			// _autoindex = true;
+		else if (*begin == "0" || *begin == "off")
+			_autoindex = false;
+		else
+			throw InvalidConfigException();
+	}
+	else
+		throw InvalidConfigException();
 }
 
 LocationConfig::LocationConfig(tokeniterator begin, tokeniterator end){
@@ -245,16 +296,26 @@ LocationConfig::LocationConfig(tokeniterator begin, tokeniterator end){
 
 std::ostream& operator<<(std::ostream& os, const LocationConfig& locationconf) {
 	// os << "++START LOCATION CONFIGURATION++" << std::endl << std::endl;
-	os << "		root		" << locationconf.getRoot() << ";" << std::endl;
-	os << "		indices		" << "TBD" << ";" << std::endl;
-	os << "		methods		" << "TBD" << ";" << std::endl;
-	os << "		redirect	" << locationconf.getRedirect() << ";" << std::endl;
-	os << "		CGI		" << locationconf.getCGI() << ";" << std::endl;
-	os << "		body_size	" << locationconf.getBodySize() << ";" << std::endl;
-	os << "		default file	" << locationconf.getDefaultFile() << ";" << std::endl;
-	os << "		upload location	" << locationconf.getUploadLocation() << ";" << std::endl;
-	os << "		CGI extension	" << locationconf.getCGIExtension() << ";" << std::endl;
-	os << "		autoindex	" << locationconf.getAutoindex() << ";" << std::endl;
+	std::vector<std::string> printbuff;
+	os << "\t\troot\t\t\t" << locationconf.getRoot() << ";" << std::endl;
+	os << "\t\tindices\t\t\t" << "TBD" << ";" << std::endl;
+	
+	os << "\t\tmethods\t\t\t";
+	printbuff = locationconf.getMethods();
+	for (std::vector<std::string>::iterator it = printbuff.begin(); it != printbuff.end(); it++){
+		os << *it;
+		if (it + 1 != printbuff.end())
+			os << " ";
+	}
+	os << ";" << std::endl;
+
+	os << "\t\tredirect\t\t" << locationconf.getRedirect() << ";" << std::endl;
+	os << "\t\tCGI\t\t\t" << locationconf.getCGI() << ";" << std::endl;
+	os << "\t\tclient_max_body_size\t" << locationconf.getBodySize() << ";" << std::endl;
+	os << "\t\tdefault file\t\t" << locationconf.getDefaultFile() << ";" << std::endl;
+	os << "\t\tupload location\t\t" << locationconf.getUploadLocation() << ";" << std::endl;
+	os << "\t\tCGI extension\t\t" << locationconf.getCGIExtension() << ";" << std::endl;
+	os << "\t\tautoindex\t\t" << locationconf.getAutoindex() << ";" << std::endl;
 	os << "	}";
 	return os;
 }
