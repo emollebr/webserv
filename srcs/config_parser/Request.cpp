826ef7b8ef01c06ec787f08868a3ea1f3e8a3040
esther <esthers@live.dk>
2024-04-25 20:02:15 +0200

    merged all 3, some problems for tomorrow

--- srcs/config_parser/Request.cpp
+++ srcs/config_parser/Request.cpp
@@ -92,6 +92,81 @@ void Request::_validateContentHeaders(size_t maxBodySize) {
     }
 }
 
+
+bool Request::isCGIRequest() {
+// Check if the request URL starts with "/cgi-bin/"
+    std::cout << "OBJECTcgi:" << _object << std::endl;
+    std::string url = _object; // Skip space and slash
+    size_t cgiPos = url.find("cgi-bin/");
+    std::cout << "IS CGI\n";
+    if (cgiPos != std::string::npos) {
+        // Request URL starts with "/cgi-bin/", consider it as CGI
+        std::cout << "IS CGI\n";
+        return true;
+    }
+    // If does not start with "/cgi-bin/", it's not a CGI request
+    return false;
+}
+
+void Request::executeCGIScript(const std::string& scriptPath, int clientSocket, char** env) {
+    // Create pipes for inter-process communication
+    
+
+    std::string path = scriptPath;
+    if (!path.empty() && path[0] == '/')
+        path = path.substr(1);
+    std::cout << "path: " << scriptPath << std::endl;
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) { // Child process
+        // Close read end of the pipe
+        close(pipefd[0]);
+
+        // Redirect standard output to the write end of the pipe
+        dup2(pipefd[1], STDOUT_FILENO);
+
+        // Close the original write end of the pipe
+        close(pipefd[1]);
+
+        char* argv[] = {(char*)path.c_str(), 0};
+
+        execve(path.c_str(), argv, env);
+
+        // If execve fails, it will continue here
+        perror("execve");
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) { // Fork failed
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else { // Parent process
+        // Close write end of the pipe
+        close(pipefd[1]);
+
+        // Read output from the pipe
+        char buffer[1024];
+        ssize_t bytesRead;
+        std::string responseData;
+        bytesRead = read(pipefd[0], buffer, sizeof(buffer));
+        responseData.append(buffer, bytesRead);
+        // Close read end of the pipe
+        close(pipefd[0]);
+
+        // Wait for the child process to finish
+        int status;
+        waitpid(pid, &status, 0);
+
+        // Send HTTP response with CGI script output
+        std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n" + responseData;
+        send(clientSocket, response.c_str(), response.size(), 0);
+    }
+}
+
 void    Request::pendingPostRequest(char* buffer, int bytesRead) {
     _body.append(std::string(buffer, bytesRead));
     _bytesReceived += bytesRead;
